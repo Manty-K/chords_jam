9559c1bdcd8340aeb1a8eeeb5d3f16cbd9895b34
Manthan Khandale <manthankhandale@gmail.com>
2022-05-21 02:28:45 +0530

    flutter 3

--- android/app/src/main/java/site/manthan/chords_jam/MainActivity.java
+++
@@ -1,6 +0,0 @@
-package site.manthan.chords_jam;
-
-import io.flutter.embedding.android.FlutterActivity;
-
-public class MainActivity extends FlutterActivity {
-}
---
+++ android/app/src/main/kotlin/com/example/chords_jam/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.chords_jam
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
